bf8a7b3f9bb00e8a27777e876be3c15dae7b5e5c
A.G <1264854+apurbagiri@users.noreply.github.com>
2021-10-18 00:31:18 +0530

    Initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import HomepageFeatures from '../components/HomepageFeatures';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+      <Box sx={{ display: 'flex' }}>
+          <Box m="auto">
+            <Avatar
+              alt="Djamaile Rahamat"
+              src={useBaseUrl('/img/profile.png')}
+              sx={{ width: 150, height: 150, m: 2 }}
+            />
+          </Box>
+        </Box>
+
+        <h1 className="hero__title">{siteConfig.title}</h1>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
